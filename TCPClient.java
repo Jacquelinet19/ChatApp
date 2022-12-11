aa50328418994c58f40ee31427769967d226fc90
michellem05 <100360350+michellem05@users.noreply.github.com>
2022-12-11 10:33:37 -0500

    Updated files

--- TCPClient.java
+++ TCPClient.java
@@ -1,27 +1,64 @@
 import java.net.*;
+import java.util.Scanner;
 import java.io.*;
 
 public class TCPClient {
     public void run() {
         try {
-            
+            Scanner scan = new Scanner(System.in);
             int server_port = 12000;
             InetAddress host = InetAddress.getByName("127.0.0.1");
             System.out.println("Server: " + server_port);
 
-            Socket socket = new Socket(host,server_port);
-            System.out.println("Connected to " + socket.getRemoteSocketAddress());
+            Socket client_socket = new Socket(host,server_port);
+            System.out.println("Connected to " + client_socket.getRemoteSocketAddress());
 
-            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter output_text = new PrintWriter(client_socket.getOutputStream(),true);
+            BufferedReader input_text = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
 
-            out.println("Hi from " + socket.getLocalSocketAddress());
-            String response = in.readLine();
-            System.out.println("Client received the message: " + response + "from Server");
-            out.close();
-            in.close();
-            socket.close();
+            Thread outgoing_text = new Thread(new Runnable() {
+                String response;
+                public void run() {
+                    while(true) {
+            
+                        response = scan.nextLine();
+                        output_text.println(response);
+                        output_text.flush();
+
+                    }
+                }
+            });
+
+            outgoing_text.start();
+            
+            Thread incoming_text = new Thread(new Runnable() {
+                String response;
+                public void run() {
+                    try {
+
+                        response = input_text.readLine();
+                        while(response != null) {
+
+                            System.out.println(response);
+                            response = input_text.readLine();
+
+                        }
+                    
+                        System.out.println("Server disconnected");
+                        output_text.close();
+                        client_socket.close();
+                        scan.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+
+                    }
+                    }
+                   
+                });
+             
+            incoming_text.start();
         }
+            
         catch (UnknownHostException e) {
             e.printStackTrace();
 
@@ -38,6 +75,7 @@ public static void main(String[] args) {
     }
 
 }
+
 
 
 
--- TCPserver.java
+++ TCPserver.java
@@ -1,41 +1,80 @@
-
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class TCPserver {
     public void run() {
         try {
+            final Scanner scan = new Scanner(System.in);
             int server_port = 12000;
             ServerSocket server_socket = new ServerSocket(server_port);
             server_socket.setSoTimeout(100000);
-            while(true) {
-                System.out.println("Waiting for client to connect" + server_socket.getLocalPort() + "...");
+            
+            System.out.println("Waiting for client to connect" + server_socket.getLocalPort() + "...");
 
-                Socket client_socket = server_socket.accept();
-                System.out.println("Just connected to " + client_socket.getRemoteSocketAddress());
+            Socket client_socket = server_socket.accept();
+            System.out.println("Just connected to " + client_socket.getRemoteSocketAddress());
 
-                PrintWriter output_text = new PrintWriter(client_socket.getOutputStream(),true);
-                BufferedReader input_text = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
-                    
-                String response = input_text.readLine();
-                System.out.println("server received: " + response);
-                output_text.println("Thanks for connecting to " + client_socket.getLocalSocketAddress());
-                server_socket.close();
-            }
+            PrintWriter output_text = new PrintWriter(client_socket.getOutputStream(),true);
+            BufferedReader input_text = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
+                
+            Thread outgoing_text = new Thread(new Runnable() {
+                    String response;
+                    public void run() {
+                        while(true) {
+
+                            response = scan.nextLine();
+                            output_text.println(response);
+                            output_text.flush();
+                        }
+                    }
+              
+                });
+            outgoing_text.start();
+            
+        
+            Thread incoming_text = new Thread(new Runnable() {
+                String response;
+                
+                public void run() {
+                    try {
+                        response = input_text.readLine();
+
+                        while(response != null) {
+                            System.out.println(response);
+                            response = input_text.readLine();
+
+                        }
+
+                        System.out.println("Client disconnected from chat");
+                        output_text.close();
+                        client_socket.close();
+                        server_socket.close();
+                        scan.close();
+                
+                    } catch (IOException i) {
+                        i.printStackTrace(); 
+
+                    }
+                }   
+            
+            });
+            incoming_text.start();
             
         }
         catch (UnknownHostException e) {
             e.printStackTrace();
 
         }
-        catch (IOException e) {
-            e.printStackTrace();
+catch (IOException i) {
+    i.printStackTrace();
 
         }
     }
 public static void main(String[] args) {
     TCPserver server = new TCPserver();
-        server.run();
+    
+    server.run();
 
     }
 }
